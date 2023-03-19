75dade81bc64ae3c6250ee13c2fc88e056be6267
Caleb Wightman <agentcboy@gmail.com>
2023-03-19 16:50:43 -0600

    removed unreferenced code

--- roverlib/Utils/PathFinder.cs
+++ roverlib/Utils/PathFinder.cs
@@ -19,21 +19,6 @@ public class PathFinder
             return (Cost + Heuristic).CompareTo(other.Cost + other.Heuristic);
         }
     }
-    public static Dictionary<(int X, int Y), int> ReadMap(string mapPath)
-    {
-        var reader = new StreamReader(mapPath);
-        string json = reader.ReadToEnd();
-        int[,] map = JsonConvert.DeserializeObject<int[,]>(json);
-        var map2 = new Dictionary<(int X, int Y), int>();
-        for (int x = 0; x < map.GetLength(0); x++)
-        {
-            for (int y = 0; y < map.GetLength(1); y++)
-            {
-                map2.Add((x, y), map[x, y]);
-            }
-        }
-        return map2;
-    }
 
     public static List<(int X, int Y)> FindPath(
         Dictionary<(int X, int Y), int> map,
@@ -105,7 +90,6 @@ public class PathFinder
         return (int)Math.Sqrt(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
     }
 
-
     public static Dictionary<(int, int), int> GetSubmap(
     Dictionary<(int, int), int> mapData,
     (int, int) start,
@@ -152,39 +136,5 @@ public class PathFinder
         var nearest = targets.OrderBy(t => Math.Abs(t.X - currentLocation.X) + Math.Abs(t.Y - currentLocation.Y)).First();
         return nearest;
     }
-    public static (int X, int Y) FindBestStart(int[,] map, (int X, int Y) target)
-    {
-        var heap = new SortedSet<Node>();
-        heap.Add(new Node { X = target.X, Y = target.Y, Cost = 0, Path = new List<(int X, int Y)> { target } });
-        var visited = new HashSet<(int X, int Y)>();
-        while (heap.Count > 0)
-        {
-            var node = heap.Min;
-            heap.Remove(node);
-            if (visited.Contains((node.X, node.Y)))
-            {
-                continue;
-            }
-            visited.Add((node.X, node.Y));
-            if (map[node.X, node.Y] == 0)
-            {
-                return (node.X, node.Y);
-            }
-            foreach (var dir in new[] { (0, 1), (0, -1), (1, 0), (-1, 0) })
-            {
-                int newX = node.X + dir.Item1;
-                int newY = node.Y + dir.Item2;
-                if (map[newX, newY] != 0)
-                {
-                    int newCost = node.Cost + map[newX, newY];
-                    var newPath = new List<(int X, int Y)>(node.Path)
-                    {
-                        (newX, newY)
-                    };
-                    heap.Add(new Node { X = newX, Y = newY, Cost = newCost, Path = newPath });
-                }
-            }
-        }
-        return (0, 0);
-    }
+    
 }
